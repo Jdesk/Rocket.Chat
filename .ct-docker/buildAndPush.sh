03d69daa7bde946dd1c24dba890ecd6989fbcdc5
Basit Mustafa <basit.mustafa@gmail.com>
2018-03-30 14:40:33 -0700

    Docker Image Build Updates

--- .ct-docker/buildAndPush.sh
+++ .ct-docker/buildAndPush.sh
@@ -1,2 +1,8 @@
+rm -fr bundle
+meteor npm install
+set +e 
+ meteor add rocketchat:lib 
+set -e
+meteor build --server-only --directory .
 docker build -t basitmustafa/ct-rocketchat -f .ct-docker/Dockerfile .
 docker push basitmustafa/ct-rocketchat 
